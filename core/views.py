1410a42b34636843e87c99d7a12eeee81bcac7f1
Rownie Rotich <zumatech1540@gmail.com>
2026-07-04 10:16:33 +0300

    Initial Lifestyle Optician project

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def appointment(request):
+    return render(request, 'appointment.html')
